93da47aba234c5236dca0e02073e506db3097e1b
Luan Cristian Thums <luancthums@gmail.com>
2020-10-06 19:39:13 -0300

    read multiple urls from file (multithreaded)

--- olx.py
+++ olx.py
@@ -11,64 +11,69 @@ class Listing:
         self.year = year
         self.price = price
         self.fipe = fipe
+        self.leilao = False
 
 def get_soup_instance(url):
-        headers = {
-                        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-                        'Accept-Language': "en,en-US;q=0,5",
-                        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8"
-                        }
+    headers = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+                'Accept-Language': "en,en-US;q=0,5",
+                'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8"
+            }
+    try:
         response = requests.get(url, headers=headers)
+    except Exception:
+        print("Failed to GET '%s'. Is it a valid URL?" % (url,))
+        return None
 
-        if response.status_code != 200:
-                print("Request ended with bad code", response.status_code)
-                return None
+    if response.status_code != 200:
+        print("Request ended with bad code", response.status_code)
+        return None
 
-        try:
-                soup = BeautifulSoup(response.text, "html.parser")
-                return soup
-        except Exception as e:
-                print("Failed to parse HTML, exception was:", str(e))
-                return None
+    try:
+        soup = BeautifulSoup(response.text, "html.parser")
+        return soup
+    except Exception as e:
+        print("Failed to parse HTML, exception was:", str(e))
+        return None
 
 class OLX():
     def __init__(self):
-            self.base_fipe_url = 'https://www.olx.com.br/tabela-fipe/'
+        self.base_fipe_url = 'https://www.olx.com.br/tabela-fipe/'
 
     def download_listing(self, url):
-            soup = get_soup_instance(url)
+        soup = get_soup_instance(url)
 
-            try:
-                    return (json.loads(soup.find('script', {'type': 'application/ld+json'}).text), url)
-            except Exception as e:
-                    return None
+        try:
+            return (json.loads(soup.find('script', {'type': 'application/ld+json'}).text), url)
+        except Exception as e:
+            return None
 
     def listing_soup_to_json(self, soup):
-            try:
-                    return json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
-            except Exception as e:
-                    return None
+        try:
+            return json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
+        except Exception as e:
+            return None
 
     def get_page(self, page_url):
-            soup = get_soup_instance(page_url)
+        soup = get_soup_instance(page_url)
 
-            f = soup.find_all('a', {'data-lurker-detail': 'list_id'})
-            ret = []
-            for result in f:
-                    ret.append(result['href'])
-            return ret
+        f = soup.find_all('a', {'data-lurker-detail': 'list_id'})
+        ret = []
+        for result in f:
+            ret.append(result['href'])
+        return ret
 
     def get_pages_from_base(self, base_url, count):
-            pages =  []
-            for page_num in range(count):
-                    suffix = '?o=' + str(page_num+1)
-                    pages.append(self.get_page(base_url+ suffix))
-            return pages
+        pages =  []
+        for page_num in range(count):
+            suffix = '?o=' + str(page_num+1)
+            pages.append(self.get_page(base_url+ suffix))
+        return pages
 
     def get_number_of_pages(self, base_url):
         soup = get_soup_instance(base_url)
-        a = soup.find('a', {'data-lurker-detail': 'last_page'})
-        if a:
+        if soup:
+            a = soup.find('a', {'data-lurker-detail': 'last_page'})
             try:
                 if a['href'].split('o=')[1].startswith('100'):
                     return 100
@@ -93,19 +98,19 @@ class OLX():
             return pages
 
     def get_page_urls(self, base_url):
-            count = self.get_number_of_pages(base_url)
-            if count == None:
-                count = 1
-            page_urls =  []
-            if '?' in base_url:
-                    add = '&o='
-            else:
-                    add = '?o='
+        count = self.get_number_of_pages(base_url)
+        if not count:
+            return None
+        page_urls =  []
+        if '?' in base_url:
+            add = '&o='
+        else:
+            add = '?o='
 
-            for page_num in range(count):
-                    suffix = add + str(page_num+1)
-                    page_urls.append(base_url + suffix)
-            return page_urls
+        for page_num in range(count):
+            suffix = add + str(page_num+1)
+            page_urls.append(base_url + suffix)
+        return page_urls
 
     def get_version_url(self, model_url, version):
         soup = get_soup_instance(model_url)
@@ -146,8 +151,13 @@ class OLX():
         if not brand or not year or not model or not model_version:
             return False
 
-        version  = model_version[len(model)+1:]
+        leilao = False
+        attributes_div = listing_soup.find_all("div", {"class": 'h3us20-5 bFJYaH'})
+        for item in attributes_div:
+            if item.text.find('leilão') != -1:
+                leilao = True
 
+        version  = model_version[len(model)+1:]
 
         fipe_url = self.base_fipe_url + brand.replace(' ', '').lower() + '/' + model.lower() + '/' + year
         url = self.get_version_url(fipe_url, model_version)
@@ -156,7 +166,9 @@ class OLX():
 
             try:
                 fipe = self.get_fipe_value(fipe_url)['props']['pageProps']['version']['price']
-                return Listing(brand, model, version, year, price, fipe)
+                lis = Listing(brand, model, version, year, price, fipe)
+                lis.leilao = leilao 
+                return lis
             except Exception as e:
                 print(str(e))
                 return None
